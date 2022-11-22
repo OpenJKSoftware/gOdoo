7151807640aafa41f51bcca92d2e3cdb93ad6170
Joshua Kreuder <Joshua_Kreuder@outlook.com>
2022-11-22 11:12:02 +0100

    [FIX] Poetry Fix Permissions: Optimize path detection

--- scripts/fix_poetry_perms.sh
+++ scripts/fix_poetry_perms.sh
@@ -2,4 +2,6 @@
 # Since we install poetry without a venv, we need to fix some permissions in order to allow the user to use Poetry in the container.
 # Since this is pretty brutal on permissions, please only use when neccessary.
 CURRUSER=$(id -u -n)
-sudo chown -R $CURRUSER:$CURRUSER {/usr/local/src/, /usr/local/lib/python3.*}
+PACKAGE_PATHS=$(python -c 'import site; print(" ".join(site.getsitepackages()))')
+echo "Chowning: $PACKAGE_PATHS"
+sudo chown -R $CURRUSER:$CURRUSER $PACKAGE_PATHS
