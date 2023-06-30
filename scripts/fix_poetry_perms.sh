3e55a7767588d3311af728a4a048eed83e01fab0
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2023-06-30 18:23:24 +0200

    [REF] Cleanup Unused Script

--- scripts/fix_poetry_perms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Since we install poetry without a venv, we need to fix some permissions in order to allow the user to use Poetry in the container.
-# Since this is pretty brutal on permissions, please only use when neccessary.
-CURRUSER=$(id -u -n)
-PACKAGE_PATHS=$(python -c 'import site; print(" ".join(site.getsitepackages()))')
-echo "Chowning: $PACKAGE_PATHS"
-sudo chown -R $CURRUSER:$CURRUSER $PACKAGE_PATHS
