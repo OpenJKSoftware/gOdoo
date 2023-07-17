cfa75fdaca6e31375b8ed27fb42dec5d15f574c5
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2023-07-17 12:20:28 +0200

    [ADD] godoo source get: stub downloading for odoo IDE extension

--- src/godoo_cli/git/git_odoo.py
+++ src/godoo_cli/git/git_odoo.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from pathlib import Path
 
@@ -26,6 +27,9 @@ def git_ensure_odoo_repo(
     odoo_url = odoo_data["url"]
     odoo_branch = odoo_data.get("branch", "master")
     odoo_commit = odoo_data.get("commit", "")
+
+    download_stubs(target_folder, force_fetch, download_archive, odoo_data, odoo_branch)
+
     if add_compare_comment:
         yaml_add_compare_commit(odoo_data, odoo_branch)
     else:
@@ -43,3 +47,43 @@ def git_ensure_odoo_repo(
     )
 
     yaml.dump(git_repos, manifest_file)
+
+
+def download_stubs(
+    target_folder: Path,
+    force_fetch: bool,
+    download_archive: bool,
+    odoo_data,
+    odoo_branch: str,
+):
+    """Download Odoo stubs for https://github.com/odoo-ide/vscode-odoo
+
+    Hopefully this Method will become obsolete in the future, when the vscode-odoo extension includes stubs in the extension itself.
+
+    Parameters
+    ----------
+    target_folder : Path
+        Stubs will be downloaded to target_folder.parent/odoo-stubs
+    force_fetch : bool
+        passed down to git_ensure_repo
+    download_archive : bool
+        passed down to git_ensure_repo
+    odoo_data : Dict
+        odoo_data from manifest.yml
+    odoo_branch : str
+        branch to download stubs from
+    """
+    odoo_stubs = odoo_data.get("stubs")
+    if odoo_stubs:
+        stub_target_path = target_folder.parent / "odoo-stubs"
+        git_ensure_repo(
+            target_folder=stub_target_path,
+            repo_src=odoo_stubs["url"],
+            branch=odoo_branch,
+            pull=force_fetch,
+            zip_mode=download_archive,
+            filter="blob:none",
+            single_branch=True,
+        )
+        if pyright_path := odoo_stubs.get("pyright_conf"):
+            json.dump({"stubPath": str(stub_target_path.absolute())}, Path(pyright_path).open("w"))
