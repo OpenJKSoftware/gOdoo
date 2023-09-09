6b274f67851f09f88cacdad751bd287fff3987bc
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2023-09-09 20:39:29 +0200

    [IMP] Dockerfile Improvements
    - Make Wkhtmltopdf script not depend on ` lsb_release`
    - Upgrade Dockerfile to enable clean prod builds (odoo venv)
    - update vscode settings and linters
    - install precommit environments in postcreate script

--- docker/install_wkhtmltopdf.sh
+++ docker/install_wkhtmltopdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RELEASE_ARCH=$(lsb_release -cs)-$(dpkg --print-architecture)
+RELEASE_NAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f 2 | tr -d '"')
+RELEASE_ARCH=$RELEASE_NAME-$(dpkg --print-architecture)
 case $RELEASE_ARCH in
     "bullseye-amd64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb" ;;
     "bullseye-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_arm64.deb" ;;
