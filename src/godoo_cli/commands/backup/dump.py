d0c7e6f80ae35442dec93e6087074aec98ab10d2
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2023-12-05 12:45:51 +0100

    ♿️ Add Odoo Version to Dump Summary

--- src/godoo_cli/commands/backup/dump.py
+++ src/godoo_cli/commands/backup/dump.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import typer
 
 from ...cli_common import CommonCLI
+from ...helpers.odoo_files import odoo_bin_get_version
 from ..db.connection import DBConnection
 
 LOGGER = logging.getLogger(__name__)
@@ -28,6 +29,7 @@ def dump_instance(
     db_port=CLI.database.db_port,
     db_user=CLI.database.db_user,
     db_password=CLI.database.db_password,
+    odoo_main_path=CLI.odoo_paths.bin_path,
     conf_path=CLI.odoo_paths.conf_path,
 ):
     """Dump DB and Filestore into Folder"""
@@ -64,6 +66,9 @@ def dump_instance(
 
     readme_path = Path(dump_path) / "README.md"
     readme_path.unlink(missing_ok=True)
+
+    odoo_version = odoo_bin_get_version(odoo_main_path)
+
     readme_content = f"""
 # gOdoo Dump
 
@@ -71,6 +76,8 @@ SQL Dump and Filestore of gOdoo Instance.
 
 ## Metadata
 
+
+- Odoo Version: {odoo_version.raw}
 - Filestore: [{ datetime.fromtimestamp(filestore_target.stat().st_mtime)}]({filestore_target.relative_to(dump_path)})
 - SQL Dump: [{ datetime.fromtimestamp(db_dump_target.stat().st_mtime)}]({db_dump_target.relative_to(dump_path)})
 
