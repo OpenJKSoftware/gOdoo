dee556f4457ba9efc4b8c8c5b59ca8ae8d6d4359
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2024-11-28 19:17:17 +0100

    ⬆️ Dev Base Image: Python 3.12

--- docker/install_wkhtmltopdf.sh
+++ docker/install_wkhtmltopdf.sh
@@ -7,8 +7,10 @@ case $RELEASE_ARCH in
     "bullseye-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_arm64.deb" ;;
     "buster-amd64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb" ;;
     "buster-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_arm64.deb" ;;
+    "bookworm-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_arm64.deb" ;;
+    "bookworm-amd64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb" ;;
+    *) echo "Cannot determine WKHTMLtoPDF version to install for rel-arch: $RELEASE_ARCH" && exit 1 ;;
 
-    *) echo "Cannot determine WKHTMLtoPDF version to install" && exit 1 ;;
 esac
 
 echo "Getting WKHTMLtoPDF from: $WKHTMLTOPDF_SOURCE"
