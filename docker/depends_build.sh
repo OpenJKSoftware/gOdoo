4f248bbceb130c0c5ca534e96d8643271652c0c4
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2025-02-25 13:17:53 +0100

    🔧 Refactor Docker Configuration and Dependency Management
    - Separate build and runtime dependencies into distinct shell scripts
    - Improve dependency caching and installation strategies
    - remove user directives for sudo

---
+++ docker/depends_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+DIR=$(dirname $(readlink -f $0))
+
+rm -f /etc/apt/apt.conf.d/docker-clean
+apt-get update
+apt-get install -y --no-install-recommends \
+    lsb-release \
+    gpg \
+    wget \
+    curl
+
+# Add PostgreSQL repository and key
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+# Download the PostgreSQL key directly instead of using a local file
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update --fix-missing
+
+# ######################### Install gOdoo dependencies #########################
+apt-get install -y -q --no-install-recommends \
+    git \
+    apt-utils \
+    openssh-client \
+    rsync \
+    unzip \
+    postgresql-client-17 \
+    libldap2-dev \
+    libssl-dev \
+    libsasl2-dev \
+    build-essential \
+    pkg-config
--- docker/depends_runtime.sh
+++ docker/depends_runtime.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-RELEASE_NAME=$(cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f 2 | tr -d '"')
-RELEASE_ARCH=$RELEASE_NAME-$(dpkg --print-architecture)
+set -e
+DIR=$(dirname $(readlink -f $0))
+
+rm -f /etc/apt/apt.conf.d/docker-clean
+apt-get update
+# ######################### Install Odoo dependencies #########################
+apt-get install -y --no-install-recommends \
+    libpq-dev \
+    imagemagick \
+    poppler-utils \
+    nginx \
+    cups-client \
+    libcupsimage2-dev \
+    libcairo2-dev
+
+# ######################### Install WKHTMLtoPDF #########################
+RELEASE_ARCH=$(lsb_release -cs)-$(dpkg --print-architecture)
 case $RELEASE_ARCH in
     "bullseye-amd64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb" ;;
     "bullseye-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_arm64.deb" ;;
@@ -10,7 +25,6 @@ case $RELEASE_ARCH in
     "bookworm-arm64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_arm64.deb" ;;
     "bookworm-amd64") WKHTMLTOPDF_SOURCE="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb" ;;
     *) echo "Cannot determine WKHTMLtoPDF version to install for rel-arch: $RELEASE_ARCH" && exit 1 ;;
-
 esac
 
 echo "Getting WKHTMLtoPDF from: $WKHTMLTOPDF_SOURCE"
