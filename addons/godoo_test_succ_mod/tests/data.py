066ff1cca2adfbd68ee57f726f9e2811cfba3f6a
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2025-06-24 13:10:43 +0200

    🚨 Fix Linter issues

--- addons/godoo_test_succ_mod/tests/data.py
+++ addons/godoo_test_succ_mod/tests/data.py
@@ -4,7 +4,6 @@ from odoo import api
 
 
 # pylint: disable=unused-argument
-# ruff: noqa: ARG001
 def generate_test_data(env: api.Environment):
     """Generate Test Data for the ven Environment."""
     # Add your Test Data Generation code here
