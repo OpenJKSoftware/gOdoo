2623ef1d7d92666fd8936a67e3800a13ee974d9e
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2025-06-24 12:32:11 +0200

    👷 Update Docker Build to deprecated apt-key

--- docker/depends_build.sh
+++ docker/depends_build.sh
@@ -7,14 +7,10 @@ rm -f /etc/apt/apt.conf.d/docker-clean
 apt-get update
 apt-get install -y --no-install-recommends \
     lsb-release \
-    gpg \
-    wget \
-    curl
+    gpg
 
-# Add PostgreSQL repository and key
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-# Download the PostgreSQL key directly instead of using a local file
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+mv $DIR/psql.gpg /etc/apt/trusted.gpg.d/
 apt-get update --fix-missing
 
 # ######################### Install gOdoo dependencies #########################
@@ -22,12 +18,14 @@ apt-get install -y -q --no-install-recommends \
     git \
     apt-utils \
     openssh-client \
+    wget \
     rsync \
+    curl \
     unzip \
     postgresql-client-17 \
     libldap2-dev \
     libssl-dev \
     libsasl2-dev \
-    libcups2-dev \
     build-essential \
-    pkg-config
+    pkg-config \
+    libcups2-dev
